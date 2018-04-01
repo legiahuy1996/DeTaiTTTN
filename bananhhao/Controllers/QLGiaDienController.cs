2a17106f008628e149dd6cf1ccd9aa1481a4a843
Your Name <you@example.com>
2018-04-01 19:49:16 +0700

    update function insert QL GiaDien

--- bananhhao/Controllers/QLGiaDienController.cs
+++ bananhhao/Controllers/QLGiaDienController.cs
@@ -20,6 +20,42 @@ namespace bananhhao.Controllers
             return View(lst);
         }
         [HttpGet]
+        public ActionResult Insert()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(giadien gd)
+        {
+            if (db.giadiens.SingleOrDefault(x => x.mabac == gd.mabac) !=null)
+            {
+                Session["ThongBao"] = "Mã bậc đã tồn tại";
+                return View("Insert");
+            }
+            //else if (dk.madk.Length != 8 || dk.makh.Length == 0 || dk.ngaysx > DateTime.Now || dk.ngaylap > DateTime.Now || dk.ngaysx >= dk.ngaylap || dk.mota == null)
+            //{
+              //  Session["ThongBao"] = "Thông tin nhập ko hợp lệ!";
+                //return View("Insert");
+            //}
+            else
+            {
+                try
+                {
+                    db.giadiens.InsertOnSubmit(gd);
+                    db.SubmitChanges();
+                    Session["ThongBao"] = "001";
+                }
+                catch (Exception e)
+                {
+                    Session["ThongBao"] = e.Message;
+                    return View("Insert");
+                }
+                return RedirectToAction("Index");
+            }
+
+        }
+        [HttpGet]
         public ActionResult Edit()
         {
             
