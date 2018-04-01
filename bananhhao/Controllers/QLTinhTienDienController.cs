1947b12e329deddacc46d7d219fc9cb944ecd16f
Phong <Phong@DESKTOP-SAM8MKA>
2018-04-01 21:24:13 +0700

    Tra cuu thong tin, còn tra cứu điện kế chưa làm

---
+++ bananhhao/Controllers/QLTinhTienDienController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bananhhao.Models;
+namespace bananhhao.Controllers
+{
+    public class QLTinhTienDienController : Controller
+    {
+        // GET: QLTinhTienDien
+        private DataClasses1DataContext db;
+        public QLTinhTienDienController()
+        {
+            db = new DataClasses1DataContext();
+        }
+        public ActionResult Index()
+        {
+            List<khachhang> lst = db.khachhangs.ToList();
+            return View(lst);
+        }
+
+    }
+}
---
+++ bananhhao/Controllers/TraCuuController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bananhhao.Models;
+namespace bananhhao.Controllers
+{
+    public class TraCuuController : Controller
+    {
+        private DataClasses1DataContext db;
+        public TraCuuController()
+        {
+            db = new DataClasses1DataContext();
+        }
+        // GET: TraCuu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult TimKiemKhachHang()
+        {
+            var kq = db.khachhangs.ToList();
+            return View(kq);
+        }
+        [HttpPost]
+        public ActionResult TimKiemKhachHang(string makh)
+        {
+            var kq = db.khachhangs.Where(x => x.makh == makh).ToList();
+            if(kq == null)
+            {
+                Session["ThongBao"] = "Không có nhân viên nào trong đây!";
+                return View("TimKiemKhachHang");
+            }
+            return View(kq);
+        }
+        [HttpGet]
+        public ActionResult TraCuuDienKe()
+        {
+            var kq = db.dienkes.ToList();
+            return View(kq);
+        }
+        [HttpPost]
+        public ActionResult TraCuuDienKe(string madk)
+        {
+            var kq = db.dienkes.Where(x => x.madk == madk).ToList();
+            if(kq == null)
+            {
+                Session["ThongBao"] = "Không tồn tại điện kế nào trong đây!";
+                return View("TraCuuDienKe");
+            }
+            return View(kq);
+        }
+    }
+}
