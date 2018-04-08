16aab5522d3ed89892b0f9de5308356a529b1974
Phong <Phong@DESKTOP-SAM8MKA>
2018-04-08 16:01:54 +0700

    hàm tính tiền điện

--- bananhhao/Controllers/QLTinhTienDienController.cs
+++ bananhhao/Controllers/QLTinhTienDienController.cs
@@ -14,11 +14,47 @@ namespace bananhhao.Controllers
         {
             db = new DataClasses1DataContext();
         }
-        public ActionResult Index()
+        public ActionResult Index(string makh,double sokwh)
         {
-            List<khachhang> lst = db.khachhangs.ToList();
+            List<khachhang> lst = db.khachhangs.Where(x=>x.makh == makh).ToList();
             return View(lst);
         }
+        public double tinhtiendien(double sokwh)
+        {
+            Double sotien;
+            if (sokwh < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                if (sokwh > 0 && sokwh <= 100)
+                {
+                    sotien = sokwh * 1.242;
+                }
+                else if (sokwh > 100 && sokwh <= 150)
+                {
+                    sotien = 100 * 1.242 + (sokwh - 100) * 1.304;
+                }
+                else if (sokwh > 150 && sokwh <= 200)
+                {
+                    sotien = 100 * 1.242 + (150 - 100) * 1.304 + (200 - 150) * 1.651;
+                }
+                else if (sokwh > 200 && sokwh <= 300)
+                {
+                    sotien = 100 * 1.242 + (150 - 100) * 1.304 + (200 - 150) * 1.651 + (300 - 200) * 1.788;
+                }
+                else if (sokwh > 300 && sokwh <= 400)
+                {
+                    sotien = 100 * 1.242 + (150 - 100) * 1.304 + (200 - 150) * 1.651 + (300 - 200) * 1.788 + (400 - 300) * 1.912;
+                }
+                else
+                {
+                    sotien = 100 * 1.242 + (150 - 100) * 1.304 + (200 - 150) * 1.651 + (300 - 200) * 1.788 + (400 - 300) * 1.912 + (sokwh - 400) * 1.962;
+                }
+            }
+            return sotien;
 
+        }
     }
 }
